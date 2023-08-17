738d8a01ce2fd8455b3f9b9218082d881777b6a1
Rumman <rummanfardeen4567@gmail.com>
2023-08-18 01:20:42 +0530

    Forms added

--- st/app/create-prompt/page.jsx
+++ st/app/create-prompt/page.jsx
@@ -1,11 +1,13 @@
 "use client"
 
-import {useState} from 'react';
+import { useState } from 'react';
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
 
 
 import Form from '@components/Form';
+import { BloomFilter } from 'next/dist/shared/lib/bloom-filter';
+import { Router } from 'next/router';
 
 const CreatePrompt = () => {
 
@@ -13,16 +15,41 @@ const CreatePrompt = () => {
 
     const [post, setPost] = useState({
         prompt: '',
-        tag:''
+        tag: ''
     });
-    return (
-        <Form 
-        type="Create"
-        post={post}
-        setPost={setPost}
-        submitting={submitting}
-        handleSubmit={CreatePrompt}></Form>
-    )
-}
 
-export default CreatePrompt
+    const createPrompt = async (e) => {
+        e.preventDefault();
+        setSubmitting(true);
+
+        try {
+            const response = await fetch('/api/create-prompt', {
+                method: 'POST',
+                body: JSON.stringify({ prompt: post.prompt, userId: sessionStorage.user.id, tag: post.tag })
+            })
+
+            if (response.ok) {
+                Router.push('/');
+            }
+        }
+        catch (error) {
+            console.log(error);
+        }
+        finally {
+            setSubmitting(false);
+        }
+
+
+
+        return (
+            <Form
+                type="Create"
+                post={post}
+                setPost={setPost}
+                submitting={submitting}
+                handleSubmit={CreatePrompt}></Form>
+
+        )
+    }
+
+    export default CreatePrompt
--- st/components/Form.jsx
+++ st/components/Form.jsx
@@ -3,12 +3,47 @@ import React from 'react'
 import Link from 'next/link'
 
 const Form = ({ type,
-post,
-setPost,
-submitting,
-handleSubmit }) => {
+    post,
+    setPost,
+    submitting,
+    handleSubmit }) => {
     return (
-        <section className="w-full max-w-full">Submit</section>
+        <section className="w-full max-w-full">
+            <h1 className="head_text text-left"><span className="blue_gradient">{type} Post</span></h1>
+            <p className="desc text-left max-w-md">
+                {type} and sgarasbkjd
+            </p>
+
+            <form
+                onSubmit={handleSubmit} className="mt-10 w-full max-w-2xl flex flex-col gap-7 glassmorphism">
+                <label>
+                    <span className="font-satoshi font-semibold text-base text-gray-700">Ur prmpt</span>
+                    <textarea
+                        value={post.prompt}
+                        onChange={(e) => { setPost({ ...post, prompt: e.target.value }) }}
+                        placeholder="Write your prompt here"
+                        className="form_textarea"></textarea>
+                </label>
+
+                <label>
+                    <span className="font-satoshi font-semibold text-base text-gray-700">Ur tag {` `}
+                        <span className="font-normal">(#webdev)</span>
+                    </span>
+                    <textarea
+                        value={post.tag}
+                        onChange={(e) => { setPost({ ...post, tag: e.target.value }) }}
+                        placeholder="Write your prompt here"
+                        className="form_textarea"
+                        required></textarea>
+                </label>
+
+                <div className="flex flex-end mx-3 mb-5 gap-4">
+                    <Link href="/" className="text-gray-500 text-sm">Cancel</Link>
+                    <button type="submit" disabled={submitting} className="px-5 py-1.5 text-sm bg-primary-orange rounded-full">Submit</button>
+                </div>
+
+            </form>
+        </section>
     )
 }
 
