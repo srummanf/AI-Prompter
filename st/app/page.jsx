843345040902ca7da6fc4282cee957595d708aec
Rumman <rummanfardeen4567@gmail.com>
2023-09-08 17:23:31 +0530

    🎨 Files modified

--- st/app/page.jsx
+++ st/app/page.jsx
@@ -6,9 +6,9 @@ const Home = () => {
         <section className="w-full flex-center flex-col">
             <h1 className="head_text text-center">Discover and Share
                 <br className="max-md:hidden"></br>
-                <span className="orange_gradient text-centre">AI poe</span>
+                <span className="orange_gradient text-centre">AI Prompt Search</span>
             </h1>
-            <p className="mt-4">Lorem ipsum dolor sit amet</p>
+            <p className="mt-4">Prompt Portal: Your Gateway to Coding, Development, and More!</p>
             <Feed></Feed>
         </section>
 
